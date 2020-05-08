f78d7f13aa6f570529847374f148be77bb67e7ac
Evgeny Korobenkov <joekor463@gmail.com>
2020-05-08 23:29:52 +0800

    print mega and kilo

---
+++ printMegaBytesAndKiloBytes.java
@@ -0,0 +1,12 @@
+public class MegaBytesConverter {
+    public static void printMegaBytesAndKiloBytes (int kiloBytes){
+        if (kiloBytes <= 0) {
+            System.out.println("Invalid value");
+        }
+        else if (kiloBytes > 0); {
+        int megaBytes = kiloBytes.intvalue(kiloBytes / 1024);
+        int kiloBytes2 = 0;
+        system.out.println(kiloBytes+"KB = "+ megaBytes + "MB and" + kiloBytes2);
+    }
+}
+
